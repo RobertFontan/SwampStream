005f61717aa687abfd5f33b166e0ecae3c143f3c
RobertFontan <87390691+RobertFontan@users.noreply.github.com>
2023-07-17 00:04:07 -0400

    set supabase client

--- src/components/NotesSidebar.jsx
+++ src/components/NotesSidebar.jsx
@@ -5,19 +5,23 @@ import Button from 'react-bootstrap/Button'
 
 function NotesSidebar() {
 
-    // get from supabase if notes exist
-    const [notes, setNotes] = useState(null)
+  // get from supabase if notes exist
+  const [notes, setNotes] = useState(null)
 
 
+  
+  
+  const handleSave = () => {
 
-    // const handleSave(e){
-    //     console.log(e)
-    // }
+    // set supabase 
+  }
+
+  // supabase pw: w9Kxb2FevOtDMDTF
 
   return (
     <div className='note-sidebar'>
-        <Button variant="outline-primary" size="sm">Save</Button>
-        <textarea rows='4' cols='50' />
+        <Button onClick={handleSave} variant="outline-primary" size="sm">Save</Button>
+        <textarea value={notes}rows='4' cols='50' onChange={(e) => setNotes(e.target.value)}/>
     </div>
   )
 }
