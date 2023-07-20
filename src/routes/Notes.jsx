3bfd0ac984f9aac84e25f0ff5fadc093b2e2dc9c
Aneesha Acharya <aneeshaacharya@Aneeshas-MacBook-Pro.local>
2023-07-20 19:17:00 -0400

    add hardcoded verticle tabs

--- src/routes/Notes.jsx
+++ src/routes/Notes.jsx
@@ -4,6 +4,8 @@ import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import TabContainer from 'react-bootstrap/TabContainer'
 
 
 import supabase from '../config/supabaseClient'
@@ -12,9 +14,6 @@ import supabase from '../config/supabaseClient'
 function Notes() {
 
   const [notes, setNotes] = useState(null)
-
-  
-
 
   useEffect(() => {
     const fetchData = async () =>{
@@ -36,28 +35,32 @@ function Notes() {
   return (
 
     <div className = "notes">
-      {notes && 
+      {/* {notes && } */}
+      <h1>
+        Notes
+      </h1>
       <div>
-
-        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-          <Row className="clearfix">
-            <Col sm={4}>
-              <Nav bsStyle="pills" stacked>
-                <NavItem eventKey="first">Tab 1</NavItem>
-                <NavItem eventKey="second">Tab 2</NavItem>
+        <Tab.Container defaultActiveKey="first">
+          <Row>
+            <Col sm={2}>
+              <Nav>
+                <Nav.Item>
+                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                </Nav.Item>
               </Nav>
             </Col>
-            <Col sm={8}>
-              <Tab.Content animation>
-                <Tab.Pane eventKey="first">Tab 1 content</Tab.Pane>
-                <Tab.Pane eventKey="second">Tab 2 content</Tab.Pane>
+            <Col>
+              <Tab.Content>
+                <Tab.Pane eventKey="first">First Tab Content</Tab.Pane>
+                <Tab.Pane eventKey="second">Second Tab Content</Tab.Pane>
               </Tab.Content>
             </Col>
           </Row>
-        </Tab.Container>;
-        
+        </Tab.Container>
       </div>
-    }
     </div>
   )
 }
