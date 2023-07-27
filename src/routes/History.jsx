79eb3e6bf36473b00ca318254ca13399a74df963
kylonocom <kylonocom@ufl.edu>
2023-07-27 18:12:46 -0400

    save button component functionality, saves and unsaves work

--- src/routes/History.jsx
+++ src/routes/History.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 
+import Video from '../components/Video'
+import supabase from '../config/supabaseClient'
+
 function History() {
+/*  // saved is array of videos 
+  const [history, setHistory] = useState(null)
+
+  // get info from supabase
+  useEffect(() =>{
+    const fetchData = async () => {
+      const {data, error} = await supabase
+      .from('History')
+      .select('*')
+
+      // updates array of saved videos
+      setHistory(data)
+
+    }
+    fetchData()
+  }, []) */
+  //{history && history.map((elementInArray) => <Video video={elementInArray}/>)}
+
+
+
+
+
   return (
-    <div>History</div>
+    <div>History
+    </div>
   )
 }
 
