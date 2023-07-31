a58938db3e254eac8cb246bad273ae72b3b9718b
RobertFontan <87390691+RobertFontan@users.noreply.github.com>
2023-07-31 14:17:36 -0400

    bullet front end done

--- client/src/components/Bullet.jsx
+++ client/src/components/Bullet.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+import {Form, Button} from 'react-bootstrap'
 
 function Bullet() {
+  const [text, setText] = useState('')
+  const [listRes, setListRes] = useState(null)
+
+  const fetchList = async (e) => {
+    e.preventDefault()
+    console.log('WE MADE IT!', text)
+    
+    try {
+      const response = await axios.post('http://localhost:5000/generate-list', { userContent: text, number: numb, option: option })
+      console.log(response.data)
+      setListRes(response.data.summary)
+    } catch (error) {
+      console.error('ai list error', error)
+    }
+  
+  
+  }
+
+
   return (
-    <div>Bullet</div>
+    <div>
+      <Form onSubmit={fetchList}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Make a Bulleted List from the following:</Form.Label>
+        <Form.Control as="textarea" rows={3} value={text} onChange={(e)=>setText(e.target.value)}/>
+        <Button type='submit'>+</Button>
+        </Form.Group>
+      </Form>
+      <div className='list-container'>
+        {listRes && <p>{listRes}</p>}
+      </div>
+    </div>
   )
 }
 
