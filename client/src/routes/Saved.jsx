f37a34789c91fdf09368a612dfc1f117b95d1b49
Robert Fontan <robertfontan@gmail.com>
2023-07-31 22:29:31 -0400

    added some frontend code

--- client/src/routes/Saved.jsx
+++ client/src/routes/Saved.jsx
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react'
+import {Button, Dropdown } from 'react-bootstrap'
+
 
 import Video from '../components/Video'
 import supabase from '../config/supabaseClient'
@@ -21,12 +23,24 @@ function Saved() {
     fetchData()
   }, []) 
 
-
-
+  // setting up filters
+  const [filter, setFilter] = useState('nothing')
 
 
   return (
-    <div>Saved
+    <div>
+      <h1>Saved</h1>
+      <Dropdown>
+        Filter by
+        <Dropdown.Toggle variant="success" id="dropdown-basic">{filter}</Dropdown.Toggle>
+    
+        <Dropdown.Menu>
+          <Dropdown.Item onClick={() => setFilter('class')}>Class</Dropdown.Item>
+          <Dropdown.Item onClick={() => setFilter('length')}>Length</Dropdown.Item>
+          <Dropdown.Item onClick={() => setFilter('date')}>Date</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
       {saved && saved.map((elementInArray) => <Video video={elementInArray}/>)}
     </div>
   )
